8129bc7dc719da19b665282e63f98d57ec9d55dd
Denis Malyshev <maldenser@gmail.com>
2023-02-17 20:18:32 +0300

    simple cookies

--- fast_api_server.py
+++ fast_api_server.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Form
+from typing import Optional
+
+from fastapi import FastAPI, Form, Cookie
 from fastapi.responses import Response
 
 app = FastAPI()
@@ -18,10 +20,15 @@ users = {
 
 
 @app.get('/')
-def index_page():
+def index_page(username: Optional[str] = Cookie(default=None)):
     with open('templates/login.html', 'r') as f:
         login_page = f.read()
-    return Response(login_page, media_type='text/html')
+    if username and users.get(username) is not None:
+        return Response(f'Привет {users[username]["name"]}!', media_type='text/html')
+    else:
+        response = Response(login_page, media_type='text/html')
+        response.delete_cookie(key="username")
+        return response
 
 
 @app.post('/login')
@@ -29,4 +36,6 @@ def process_login_page(username = Form(...), password = Form(...)):
     user = users.get(username)
     if not user or user['password'] != password:
         return Response('Я вас не знаю', media_type='text/html')
-    return Response(f'Привет {username}, твой баланс {user["balance"]}', media_type='text/html')
+    response = Response(f'Привет {username}, твой баланс {user["balance"]}', media_type='text/html')
+    response.set_cookie(key='username', value=username)
+    return response
