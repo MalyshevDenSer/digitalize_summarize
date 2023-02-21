54845a399900aa57f3e877798f15613412ea1eb2
Denis Malyshev <maldenser@gmail.com>
2023-02-21 19:52:29 +0300

    returning Response object insted of str data, PHONE_FORMAR become tuple, not dict

--- unify_phone_from_json.py
+++ unify_phone_from_json.py
@@ -1,7 +1,8 @@
-from fastapi import Request, FastAPI
+from fastapi import Request, Response, FastAPI
 
 app = FastAPI()
-PHOHE_FORMAT = {1: ' ', 2: '(', 6: ')', 7: ' ', 11: '-', 14: '-'}
+PHONE_FORMAT = ((1, ' '), (2, '('), (6, ')'), (7, ' '), (11, '-'), (14, '-'))
+
 
 
 @app.post('/unify_phone_from_json')
@@ -16,11 +17,11 @@ async def main(info: Request):
 def unify_phone_number(phone_sybmols):
     list_of_digits = [i for i in phone_sybmols if i.isdigit()]
     if list_of_digits[0] not in ['7', '8', '9']:
-        return ''.join(list_of_digits)
+        return Response(''.join(list_of_digits), media_type='text/html')
     if list_of_digits[0] == '9':
         list_of_digits.insert(0, '8')
     elif list_of_digits[0] == '7':
         list_of_digits[0] = '8'
-    for i in PHOHE_FORMAT.keys():
-        list_of_digits.insert(i, PHOHE_FORMAT.get(i))
-    return''.join(list_of_digits)
+    for i in PHONE_FORMAT:
+        list_of_digits.insert(i[0], i[1])
+    return Response(''.join(list_of_digits), media_type='text/html')
